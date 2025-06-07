d9ff98a0d7f3cc4b37fddaef90675f45c95768b8
Mathfw <mjbferreira2504@gmail.com>
2025-06-06 21:54:25 -0300

    feat: add css classes to CustomLegend

--- src/components/CustomLegend.tsx
+++ src/components/CustomLegend.tsx
@@ -1,18 +1,20 @@
-import type { LegendProps } from "recharts"
-import { useTranslation } from "react-i18next";
+import './CustomLegend.css';
+
+import type { LegendProps } from 'recharts';
+import { useTranslation } from 'react-i18next';
 
 export const CustomLegend: React.FC<LegendProps> = ({ payload }) => {
 
-  const [t, i18n] = useTranslation();
+  const [t] = useTranslation();
 
   return (
-    <ul style={{display: 'flex', width: 'full', alignItems: 'center', justifyContent: 'center', gap: '1.5em'}}>
+    <ul className='legend'>
       {
         payload?.map((v, i) => {
           if (v.value === 'gross') {
-            return <li key={i}>{t('earnings')}</li>
+            return <li className='legend__item' key={i}>{t('earnings')}</li>
           } else if (v.value === 'invested') {
-            return <li key={i}>{t('invested')}</li>
+            return <li className='legend__item' key={i}>{t('invested')}</li>
           }
         })
       }
